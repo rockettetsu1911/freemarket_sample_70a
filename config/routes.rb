5f1a94da2b5660fb115cd3af613253c04134d9af
gohsan53 <s.kitagoh@gmail.com>
2020-05-10 10:58:51 +0900

    ルーティングをresourcesを使用する記述に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get 'items/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, only: :index
 end
