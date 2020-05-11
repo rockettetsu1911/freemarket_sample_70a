f2ab6040ef4d370d299605bde6db812556ef996a
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-11 22:51:49 +0900

    pictureのマイグレーションファイルの記載
    カラムと型の記載

--- db/migrate/20200511080949_create_pictures.rb
+++ db/migrate/20200511080949_create_pictures.rb
@@ -1,7 +1,8 @@
 class CreatePictures < ActiveRecord::Migration[5.0]
   def change
     create_table :pictures do |t|
-
+      t.string :image,    null:false
+      t.references :item, null:false, foreign_key:true
       t.timestamps
     end
   end
