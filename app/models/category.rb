017141446ea25dd883a64f64e4df6565dabee714
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-12 23:44:53 +0900

    categoriesテーブルにenumの記載

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,23 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
 
-  validates :name, presence: { message: "商品名を入力してください" }
+  validates :name, presence: { message: "カテゴリーを選択してください" }
+
+  enum name:{
+    '---':                 0,
+    radies:                1, 
+    mens:                  2, 
+    baby_and_kids:         3,
+    interior:              4, 
+    sub_culture:           5, 
+    hobby:                 6,
+    cosme:                 7,
+    electrical_appliances: 8,
+    sports:                9,
+    handmade:              10,
+    ticket:                11,
+    bicycle:               12,
+    etc:                   13
+  }
 
 end
