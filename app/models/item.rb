c6e736c10fb99b1d122d7cdae45d58f291b40f19
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-12 19:28:25 +0900

    itemモデルの記載(アソシエーション)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_many   :tags, through: :item, dependent: :destroy
+  has_many   :item_tags
+  belongs_to :category
+  has_many   :comments,             dependent: :destroy
+  has_many   :pictures,             dependent: :destroy
+  has_many   :likes,                dependent: :destroy
+
 end
