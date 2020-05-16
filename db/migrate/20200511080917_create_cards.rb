4fb26ad5a834068516502b5f4c4068e3dbb691af
kbandai20200118 <61614549+kbandai20200118@users.noreply.github.com>
2020-05-16 16:26:56 +0900

    AWSデプロイ　自動デプロイ

--- db/migrate/20200511080917_create_cards.rb
+++ db/migrate/20200511080917_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration[5.0]
     create_table :cards do |t|
       t.string    :customer_id, null:false
       t.string    :card_id,     null:false
-      t.referenes :user,        null:false, foreign_key:true
+      t.references :user,        null:false, foreign_key:true
       t.timestamps
     end
   end
