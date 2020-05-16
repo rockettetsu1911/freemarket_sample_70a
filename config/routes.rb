d6f9b50ad2cb6605d8fd39b00d1bb6fcec2465a6
atyoshimatsu <at.yoshimatsu@gmail.com>
2020-05-16 16:29:29 +0900

    routesへアクションを記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'users/log_in', :to => 'users#login'
+    get 'users/sign_up', :to => 'users#signup'
+  end
   devise_for :users
+
   root 'items#index'
   resources :items, only: :index
 end
